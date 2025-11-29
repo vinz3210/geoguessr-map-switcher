065bc4a0b1a00b40ae3a6690944ec899f9da6422
Vinz3210 <166544727+vinz3210@users.noreply.github.com>
2025-11-29 23:10:00 +0100

    incerased version number

--- geoguessr-map-switcher.user.js
+++ geoguessr-map-switcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Geoguessr Map Switcher
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  Switch to OpenStreetMap, OpenTopoMap, etc. in Geoguessr
 // @author       vinz3210
 // @match        https://www.geoguessr.com/*
